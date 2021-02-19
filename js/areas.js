6ea60631729c5314f4485c4e423ee4c0ca504322
niko5778 <lozanonicolas65@gmail.com>
2021-02-19 17:23:17 -0500

    itegracion calculadora

--- js/areas.js
+++ js/areas.js
@@ -160,7 +160,116 @@ function obtener() {
 
     document.getElementById("resultados").innerHTML = r;
 }
+/*calculadora*/ 
+var operandoa = 0;
+var operandob = 0;
+var operacion = 0;
 
-
+function init(){
+    //variables
+    var final = document.getElementById('final');
+    var Acero = document.getElementById('Acero');
+    var suma = document.getElementById('suma');
+    var resta = document.getElementById('resta');
+    var multiplicacion = document.getElementById('multiplicacion');
+    var division = document.getElementById('division');
+    var igual = document.getElementById('igual');
+    var uno = document.getElementById('uno');
+    var dos = document.getElementById('dos');
+    var tres = document.getElementById('tres');
+    var cuatro = document.getElementById('cuatro');
+    var cinco = document.getElementById('cinco');
+    var seis = document.getElementById('seis');
+    var siete = document.getElementById('siete');
+    var ocho = document.getElementById('ocho');
+    var nueve = document.getElementById('nueve');
+    var cero = document.getElementById('cero');
+    //Eventos de click
+  uno.onclick = function(e){
+    final.textContent = final.textContent  + "1";
+}
+dos.onclick = function(e){
+    final.textContent = final.textContent  + "2";
+}
+tres.onclick = function(e){
+    final.textContent = final.textContent  + "3";
+}
+cuatro.onclick = function(e){
+    final.textContent = final.textContent  + "4";
+}
+cinco.onclick = function(e){
+    final.textContent = final.textContent  + "5";
+}
+seis.onclick = function(e){
+    final.textContent = final.textContent  + "6";
+}
+siete.onclick = function(e){
+    final.textContent = final.textContent  + "7";
+}
+ocho.onclick = function(e){
+    final.textContent = final.textContent  + "8";
+}
+nueve.onclick = function(e){
+    final.textContent = final.textContent  + "9";
+}
+cero.onclick = function(e){
+    final.textContent = final.textContent  + "0";
+}
+Acero.onclick = function(e){
+    resetear();
+}
+suma.onclick = function(e){
+    operandoa = final.textContent;
+    operacion = "+";
+    limpiar();
+}
+resta.onclick = function(e){
+    operandoa = final.textContent;
+    operacion = "-";
+    limpiar();
+}
+multiplicacion.onclick = function(e){
+    operandoa = final.textContent;
+    operacion = "*";
+    limpiar();
+}
+division.onclick = function(e){
+    operandoa = final.textContent;
+    operacion = "/";
+    limpiar();
+}
+igual.onclick = function(e){
+    operandob = final.textContent;
+    resolver();
+}
+}
+function limpiar(){
+    final.textContent = "";
+  }
+  function resetear(){
+    final.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+  }
+  function resolver(){
+    var res = 0;
+    switch(operacion){
+      case "+":
+        res = parseFloat(operandoa) + parseFloat(operandob);
+        break;
+      case "-":
+          res = parseFloat(operandoa) - parseFloat(operandob);
+          break;
+      case "*":
+        res = parseFloat(operandoa) * parseFloat(operandob);
+        break;
+      case "/":
+        res = parseFloat(operandoa) / parseFloat(operandob);
+        break;
+    }
+    resetear();
+    final.textContent = res;
+  }
 
     
